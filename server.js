ef51e61f4209ddc2c06d3c25661b57cddad80ba1
davide_cz <carletti.dav@gmail.comm>
2025-06-14 15:43:36 +0200

    commit per deploy

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
     origin: ['http://localhost:5173', 'https://tattoo-mee-web-app.vercel.app'], // Consenti il frontend locale e deployato
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Metodi HTTP consentiti
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    credentials: true// aggiunto con credential
 }));
 
 app.use(morgan('dev'));
